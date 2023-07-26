b010c33800297439288355e58f8e7fdd30275c24
Leonardo Araujo <leo.san9@hotmail.com>
2023-07-26 14:47:39 -0300

    build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Col, Row } from "antd";
 import { useMediaQuery } from "react-responsive";
-import AnimatedTextCharacter from "../components/TextSpring";
 import "./App.css";
 import { Desktop } from "./components/desktop";
 import { Mobile } from "./components/mobile";
